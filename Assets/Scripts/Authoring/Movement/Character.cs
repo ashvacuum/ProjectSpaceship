d508723f37ed3f6abb9acaa278f5f532a9d58fd5
Brian <gol.rouge@gmail.com>
2024-10-02 22:23:40 +0800

    Movement system ECS
    also pushed here is the Editor Settings "do not reload domain or Scene"

---
+++ Assets/Scripts/Authoring/Movement/Character.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using Unity.Entities;
+
+public class Character : MonoBehaviour
+{
+    public float speed = 2f;
+
+
+}
+
+public struct CharacterData : IComponentData 
+{
+    public float speed;
+}
+
+public class CharacterBaker : Baker<Character>
+{
+    public override void Bake(Character authoring)
+    {
+        var entity = GetEntity(TransformUsageFlags.Dynamic);
+        AddComponent(entity, new CharacterData
+        {
+            speed = authoring.speed
+        });
+
+        AddComponent(entity, new InputsData 
+        { 
+        
+        });
+    }
+}
---
+++ Assets/Scripts/Authoring/Movement/InputsData.cs
@@ -0,0 +1,8 @@
+using Unity.Entities;
+using UnityEngine;
+using Unity.Mathematics;
+
+public struct InputsData : IComponentData
+{
+    public float2 move;
+}
