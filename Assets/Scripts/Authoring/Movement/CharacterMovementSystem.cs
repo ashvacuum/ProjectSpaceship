ce6117d6d9ee6d6ebd43d982aacf4948d0111f21
Brian <gol.rouge@gmail.com>
2024-10-03 22:17:14 +0800

    Moving Character to face the forward direction

--- Assets/Scripts/Authoring/Movement/CharacterMovementSystem.cs
+++ Assets/Scripts/Authoring/Movement/CharacterMovementSystem.cs
@@ -11,9 +11,19 @@ public partial struct CharacterMovementSystem : ISystem
                  SystemAPI.Query<RefRO<CharacterData>, RefRO<InputsData>, RefRW<LocalTransform>>())
         {
             float3 position = transform.ValueRO.Position;
-            position.x += inputs.ValueRO.move.x * data.ValueRO.speed * SystemAPI.Time.DeltaTime;
-            position.y += inputs.ValueRO.move.y * data.ValueRO.speed * SystemAPI.Time.DeltaTime;
+            
+            float3 moveDirection = new float3(inputs.ValueRO.move.x, 0, inputs.ValueRO.move.y); // Assuming y is up
+            position += moveDirection * data.ValueRO.speed * SystemAPI.Time.DeltaTime;
+            
             transform.ValueRW.Position = position;
+            
+          
+            
+            if (math.length(moveDirection) > 0.01f) // Only rotate if there's significant movement
+            {
+                quaternion rotation = quaternion.LookRotationSafe(math.normalize(moveDirection), math.up()); // Use the up vector for rotation
+                transform.ValueRW.Rotation = rotation;
+            }
         }
     }
 }
